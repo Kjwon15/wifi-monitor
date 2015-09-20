2396746b42d3a5f435e048452b5ae9d16bf68c7c
kjwon15 <kjwonmail@gmail.com>
2015-09-20 10:12:22 +0900

    Speak asynchronously

--- wifimonitor/tts.py
+++ wifimonitor/tts.py
@@ -1,12 +1,30 @@
 import gtts
 import tempfile
 import subprocess
+import threading
+import Queue
+
+
+speak_queue = Queue.Queue()
+
+def _speak():
+    while 1:
+        string, lang = speak_queue.get()
+        try:
+            t = gtts.gTTS(string, lang=lang)
+            f = tempfile.NamedTemporaryFile()
+            t.write_to_fp(f)
+            f.flush()
+            subprocess.Popen(['mpg321', '-q', f.name]).wait()
+            f.close()
+        finally:
+            speak_queue.task_done()
 
 
 def speak(string, lang='en'):
-    t = gtts.gTTS(string, lang=lang)
-    f = tempfile.NamedTemporaryFile()
-    t.write_to_fp(f)
-    f.flush()
-    subprocess.Popen(['mpg321', '-q', f.name]).wait()
-    f.close()
+    speak_queue.put((string, lang))
+
+
+thread = threading.Thread(target=_speak)
+thread.setDaemon(True)
+thread.start()
