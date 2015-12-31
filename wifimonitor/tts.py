8af324f9454baa0770f75ec356f63a2852f3d046
kjwon15 <kjwonmail@gmail.com>
2016-01-01 03:14:07 +0900

    Use voicerss

--- wifimonitor/tts.py
+++ wifimonitor/tts.py
@@ -1,17 +1,23 @@
 import Queue
 import hashlib
 import os
+import requests
 import subprocess
 import tempfile
 import threading
 
-import gtts
 import pyttsx
+
+
+API_URL = 'https://api.voicerss.org/'
+API_KEY = '80b6bc3bffb3432caf35b54b5078e2e3'
 
 
 speak_queue = Queue.Queue()
 engine = pyttsx.init()
-cachedir = tempfile.mkdtemp(prefix='wifimon')
+cachedir = os.path.join(tempfile.gettempdir(), 'wifimon')
+if not os.path.exists(cachedir):
+    os.mkdir(cachedir)
 
 
 def _speak():
@@ -23,18 +29,28 @@ def _speak():
             speak_queue.task_done()
         else:
             try:
-                t = gtts.gTTS(string, lang=lang)
-                t.save(filename)
-            except:
+                resp = requests.get(API_URL, {
+                    'key': API_KEY,
+                    'src': string,
+                    'hl': lang,
+                    'f': '44khz_16bit_mono',
+                })
+                if resp.headers['Content-Type'].startswith('text'):
+                    raise Exception('API error')
+                with open(filename, 'w') as fp:
+                    fp.write(resp.content)
+            except Exception as e:
+                print(repr(e))
                 engine.say(string)
                 engine.runAndWait()
+
             else:
                 subprocess.Popen(['mpg321', '-q', filename]).wait()
             finally:
                 speak_queue.task_done()
 
 
-def speak(string, lang='en'):
+def speak(string, lang='en-us'):
     speak_queue.put((string, lang))
 
 
