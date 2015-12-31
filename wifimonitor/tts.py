b90f45e0177ca9202cc13011d9dfb5c265ed3bea
kjwon15 <kjwonmail@gmail.com>
2016-01-01 03:40:13 +0900

    Use ThreadPool to speak

--- wifimonitor/tts.py
+++ wifimonitor/tts.py
@@ -3,41 +3,38 @@ import hashlib
 import os
 import subprocess
 import tempfile
-import threading
 
 import gtts
 import pyttsx
 
+from multiprocessing.pool import ThreadPool
 
-speak_queue = Queue.Queue()
+
+pool = ThreadPool()
 engine = pyttsx.init()
 cachedir = tempfile.mkdtemp(prefix='wifimon')
 
 
-def _speak():
-    while 1:
-        string, lang = speak_queue.get()
-        filename = os.path.join(cachedir, hashlib.md5(string).hexdigest())
-        if os.path.exists(filename):
-            subprocess.Popen(['mpg321', '-q', filename]).wait()
-            speak_queue.task_done()
+def async(decorated):
+
+    def send(*args, **kwargs):
+        return pool.apply_async(decorated, args, kwargs)
+
+    return send
+
+
+@async
+def speak(string, lang):
+    filename = os.path.join(cachedir, hashlib.md5(string).hexdigest())
+    if os.path.exists(filename):
+        subprocess.Popen(['mpg321', '-q', filename]).wait()
+    else:
+        try:
+            t = gtts.gTTS(string, lang=lang)
+            t.save(filename)
+        except:
+            engine.say(string)
+            engine.runAndWait()
         else:
-            try:
-                t = gtts.gTTS(string, lang=lang)
-                t.save(filename)
-            except:
-                engine.say(string)
-                engine.runAndWait()
-            else:
-                subprocess.Popen(['mpg321', '-q', filename]).wait()
-            finally:
-                speak_queue.task_done()
+            subprocess.Popen(['mpg321', '-q', filename]).wait()
 
-
-def speak(string, lang='en'):
-    speak_queue.put((string, lang))
-
-
-thread = threading.Thread(target=_speak)
-thread.setDaemon(True)
-thread.start()
